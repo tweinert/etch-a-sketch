e6ee339aefde94d3c6d64e630bdf4e08bd83cd54
tweinert <tait.weinert@outlook.com>
2021-11-09 07:03:46 +1030

    basic reset and create function

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
 const gridContainer = document.querySelector('.gridContainer');
 
-// create grid using 2 for loops
-for (let i = 0; i < 16; i++) {
-    let gridRowDiv = document.createElement('div');
-    gridRowDiv.classList.add('gridRow');
-    gridContainer.appendChild(gridRowDiv);
-    for (let j = 0; j < 16; j++) {
-        let gridSquareDiv = document.createElement('div');
-        gridSquareDiv.classList.add('gridSquare');
-        gridRowDiv.appendChild(gridSquareDiv);
-    }
-}
+const maxGridPixels = 960;
 
 // change colour on hover event
 const squares = document.querySelectorAll('.gridSquare');
 squares.forEach(key => key.addEventListener('mouseover', addColour));
-squares.forEach(key => key.addEventListener('mouseout', removeColour));
+//squares.forEach(key => key.addEventListener('mouseout', removeColour));
 
+window.onload = function() {
+    createGrid(16);
+};
+
+// adds colour to square, used when hover
 function addColour(e) {
     this.classList.add('grey');
 }
 
 function removeColour(e) {
     this.classList.remove('grey');
+}
+
+// ask user for grid size
+function promptSize() {
+    let squaresPerSide = prompt("How many squares per side?");
+    createGrid(squaresPerSide);
+}
+
+// create grid with given number of squares per side
+function createGrid(squaresPerSide) {
+    // clear grid
+    gridContainer.innerHTML = '';
+
+    // square size string
+    
+
+    for (let i = 0; i < squaresPerSide; i++) {
+        let gridRowDiv = document.createElement('div');
+        gridRowDiv.classList.add('gridRow');
+        gridContainer.appendChild(gridRowDiv);
+        for (let j = 0; j < squaresPerSide; j++) {
+            let gridSquareDiv = document.createElement('div');
+            gridSquareDiv.classList.add('gridSquare');
+            gridSquareDiv.style.height = "48px";
+            gridSquareDiv.style.width = "48px";
+            gridRowDiv.appendChild(gridSquareDiv);
+        }
+    }
 }
