db2d6f72ae5f7a0efa09d7446de1306f0d718e02
tweinert <tait.weinert@outlook.com>
2021-11-08 18:11:03 +1030

    grid complete

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const gridContainer = document.querySelector('.gridContainer');
 
 for (let i = 0; i < 16; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("gridSquare");
-    gridContainer.appendChild(newDiv);
+    let gridRowDiv = document.createElement('div');
+    gridRowDiv.classList.add('gridRowDiv');
+    gridContainer.appendChild(gridRowDiv);
+    for (let j = 0; j < 16; j++) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('gridSquare');
+        gridRowDiv.appendChild(newDiv);
+    }
 }
