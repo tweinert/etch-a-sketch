c4dec9350b22dde873d8a9c6b12530c47f98a0b1
tweinert <tait.weinert@outlook.com>
2021-11-11 17:35:36 +1030

    random colour, some refactoring

--- script.js
+++ script.js
@@ -9,24 +9,6 @@ window.onload = function() {
     createGrid(squaresPerSide);
 };
 
-// adds colour to square, used when hover
-function addColour(e) {
-    this.classList.add('grey');
-}
-
-function removeColour(e) {
-    this.classList.remove('grey');
-}
-
-// ask user for grid size
-function promptSize() {
-    squaresPerSide = 0;
-    while(isNaN(squaresPerSide) || squaresPerSide > 100 || squaresPerSide < 2) {
-        squaresPerSide = prompt("How many squares per side?");
-    }
-    createGrid(squaresPerSide);
-}
-
 // create grid with given number of squares per side
 function createGrid(squaresPerSide) {
     // clear grid
@@ -36,26 +18,43 @@ function createGrid(squaresPerSide) {
     let squareSize = maxGridPixels / squaresPerSide;
 
     for (let i = 0; i < squaresPerSide; i++) {
+        // row container div
         let gridRowDiv = document.createElement('div');
         gridRowDiv.classList.add('gridRow');
         gridRowDiv.style.height = squareSize + "px";
         gridContainer.appendChild(gridRowDiv);
 
         for (let j = 0; j < squaresPerSide; j++) {
+            // individual square div
             let gridSquareDiv = document.createElement('div');
             gridSquareDiv.classList.add('gridSquare');
+
+            // set styling
             gridSquareDiv.style.height = squareSize + "px";
             gridSquareDiv.style.width = squareSize + "px";
-            gridRowDiv.appendChild(gridSquareDiv);
             if (i === squaresPerSide - 1) {
                 gridSquareDiv.style.borderBottom = "1px solid black";
             }
+
+            gridRowDiv.appendChild(gridSquareDiv);
+            
         }
     }
 
     // add mouseover event
     let squares = document.querySelectorAll('.gridSquare');
-    squares.forEach(key => key.addEventListener('mouseover', addColour));
+    squares.forEach(key => key.addEventListener('mouseover', addRandomColour));
+}
+
+// sets background to random colour using getRandomRGB function
+function addRandomColour(e) {
+    // only do this on first mouseover
+    if (this.classList.contains('randomRGB')) {
+        return;
+    }
+
+    this.classList.add('randomRGB');
+    this.style.backgroundColor = getRandomColour();
 }
 
 function getRandomColour() {
@@ -67,8 +66,11 @@ function getRandomColour() {
     return randomRGB;
 }
 
-
-/*
-TODO
-    use getRandomColour() to select colour on mouseover
-*/
+// ask user for grid size
+function promptSize() {
+    squaresPerSide = 0;
+    while(isNaN(squaresPerSide) || squaresPerSide > 100 || squaresPerSide < 2) {
+        squaresPerSide = prompt("How many squares per side?");
+    }
+    createGrid(squaresPerSide);
+}
